64d90f87143aef7db4dfe4b64327554b5e6d900d
igorurbaniak <igorurbaniakq@wp.pl>
2024-02-19 18:34:52 +0100

    Add more UI content to the short dishes discription

--- lib/screens/menu_meal_screen.dart
+++ lib/screens/menu_meal_screen.dart
@@ -14,122 +14,190 @@ class MenuScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                height: 55,
-                width: MediaQuery.sizeOf(context).width / 2,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey.shade300,
-                    width: 1,
-                  ),
-                ),
-                child: TextButton.icon(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                  icon: const Icon(Icons.star),
-                  label: const Text('Popular'),
-                ),
-              ),
-              Container(
-                height: 55,
-                width: MediaQuery.sizeOf(context).width / 2,
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey.shade300,
-                    width: 1,
-                  ),
-                ),
-                child: TextButton.icon(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(padding: EdgeInsets.zero),
-                  icon: const Icon(Icons.favorite),
-                  label: const Text('Favorites: 0'),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 150, width: double.infinity),
-          Container(
-            color: Colors.grey.shade100,
-            padding: const EdgeInsets.only(left: 15),
-            height: 100,
-            width: double.infinity,
-            child: const Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
               children: [
-                Text(
-                  'Śniadania 🥐',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+                Container(
+                  height: 55,
+                  width: MediaQuery.sizeOf(context).width / 2,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey.shade300,
+                      width: 1,
+                    ),
+                  ),
+                  child: TextButton.icon(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(padding: EdgeInsets.zero),
+                    icon: const Icon(Icons.star),
+                    label: const Text('Popular'),
                   ),
                 ),
-                Text(
-                  'od 8:00 - 17:00',
-                  style: TextStyle(
-                    fontSize: 14,
+                Container(
+                  height: 55,
+                  width: MediaQuery.sizeOf(context).width / 2,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey.shade300,
+                      width: 1,
+                    ),
+                  ),
+                  child: TextButton.icon(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(padding: EdgeInsets.zero),
+                    icon: const Icon(Icons.favorite),
+                    label: const Text('Favorites: 0'),
                   ),
                 ),
               ],
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(15),
-            height: 300,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.grey.shade300,
-                width: 1,
-              ),
-            ),
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.asset(
-                      'images/crossaint.jpg',
-                      fit: BoxFit.cover,
-                      height: 100,
-                      width: 140,
+            const SizedBox(height: 150, width: double.infinity),
+            Container(
+              color: Colors.grey.shade100,
+              padding: const EdgeInsets.only(left: 15),
+              height: 100,
+              width: double.infinity,
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Śniadania 🥐',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
+                  Text(
+                    'od 8:00 - 17:00',
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Column(
+              children: [
+                DishDiscription(),
+                DishDiscription(),
+                DishDiscription(),
+                DishDiscription(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DishDiscription extends StatelessWidget {
+  const DishDiscription({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      height: 260,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey.shade300,
+          width: 1,
+        ),
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            right: 0,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset(
+                'images/crossaint.jpg',
+                fit: BoxFit.cover,
+                height: 100,
+                width: 140,
+              ),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Wspomnienie\nznad morza',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 5),
+              const Text(
+                '28 PLN',
+                style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.brown),
+              ),
+              const SizedBox(height: 5),
+              const Text(
+                'croissant, łosoś, kremowy serek\nlabne, pomidorki koktajolwe\nkapary, rukola, sos cytrynowy,\nsmażone nasiona',
+                style: TextStyle(
+                  fontSize: 14,
                 ),
-                const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Wspomnienie\nznad morza',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 5),
-                    Text(
-                      '28 PLN',
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.brown),
-                    ),
-                    SizedBox(height: 5),
-                    Text(
-                      'croissant, łosoś, kremowy serek\nlabne, pomidorki koktajolwe\nkapary, rukola, sos cytrynowy,\nsmażone nasiona',
-                      style: TextStyle(
-                        fontSize: 14,
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  const Icon(Icons.access_time, size: 16),
+                  const SizedBox(width: 5),
+                  const Text(
+                    '16min',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  const SizedBox(width: 30),
+                  const Icon(Icons.thumb_up_alt_outlined, size: 16),
+                  const SizedBox(width: 5),
+                  const Text(
+                    'Polecany',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  const SizedBox(width: 108),
+                  Container(
+                    height: 40,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                        color: Colors.grey.shade300,
+                        width: 1,
                       ),
                     ),
-                  ],
-                ),
-              ],
-            ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 40,
+                          width: 50,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(Icons.favorite_outline_outlined),
+                              Center(
+                                child: Text('20'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+            ],
           ),
         ],
       ),
