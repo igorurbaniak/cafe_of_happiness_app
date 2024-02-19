924de4dbad89a7cb830c00f4fea80497d6f8c20d
igorurbaniak <igorurbaniakq@wp.pl>
2024-02-19 15:26:54 +0100

    Create the look of Dishes after selecting the category in the Menu

--- lib/screens/menu_meal_screen.dart
+++ lib/screens/menu_meal_screen.dart
@@ -19,12 +19,12 @@ class MenuScreen extends StatelessWidget {
           Row(
             children: [
               Container(
-                height: 60,
+                height: 55,
                 width: MediaQuery.sizeOf(context).width / 2,
                 decoration: BoxDecoration(
                   border: Border.all(
                     color: Colors.grey.shade300,
-                    width: 0.5,
+                    width: 1,
                   ),
                 ),
                 child: TextButton.icon(
@@ -35,12 +35,12 @@ class MenuScreen extends StatelessWidget {
                 ),
               ),
               Container(
-                height: 60,
+                height: 55,
                 width: MediaQuery.sizeOf(context).width / 2,
                 decoration: BoxDecoration(
                   border: Border.all(
                     color: Colors.grey.shade300,
-                    width: 0.5,
+                    width: 1,
                   ),
                 ),
                 child: TextButton.icon(
@@ -52,7 +52,85 @@ class MenuScreen extends StatelessWidget {
               ),
             ],
           ),
-          const SizedBox(height: 100, width: double.infinity),
+          const SizedBox(height: 150, width: double.infinity),
+          Container(
+            color: Colors.grey.shade100,
+            padding: const EdgeInsets.only(left: 15),
+            height: 100,
+            width: double.infinity,
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Śniadania 🥐',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'od 8:00 - 17:00',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(15),
+            height: 300,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey.shade300,
+                width: 1,
+              ),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      'images/crossaint.jpg',
+                      fit: BoxFit.cover,
+                      height: 100,
+                      width: 140,
+                    ),
+                  ),
+                ),
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Wspomnienie\nznad morza',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      '28 PLN',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.brown),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      'croissant, łosoś, kremowy serek\nlabne, pomidorki koktajolwe\nkapary, rukola, sos cytrynowy,\nsmażone nasiona',
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
