bfa5041cd59ca33761e99b9dda259cbd1d672c3d
igorurbaniak <igorurbaniakq@wp.pl>
2024-05-11 15:30:19 +0200

    Change CircularProgressIndicator to Shimmer effect in HomeMenuCategory when loading MENU categories

--- lib/presentation/features/home/home_page/widgets/home_menu_category/home_menu_category.dart
+++ lib/presentation/features/home/home_page/widgets/home_menu_category/home_menu_category.dart
@@ -6,6 +6,7 @@ import 'package:cafe_of_happiness_app/presentation/features/home/home_page/widge
 import 'package:cafe_of_happiness_app/presentation/features/menu_item/menu_item_page/menu_item_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomeMenuCategory extends StatelessWidget {
   const HomeMenuCategory({
@@ -63,8 +64,48 @@ class HomeMenuCategory extends StatelessWidget {
                       child: Text('Initial state'),
                     );
                   case Status.loading:
-                    return const Center(
-                      child: CircularProgressIndicator(),
+                    return Shimmer.fromColors(
+                      baseColor: Colors.grey.shade300,
+                      highlightColor: Colors.grey.shade100,
+                      child: ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: 6,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 5),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  width: 54,
+                                  height: 54,
+                                  color: Colors.white,
+                                ),
+                                const SizedBox(width: 10),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        height: 8.0,
+                                        color: Colors.white,
+                                      ),
+                                      const SizedBox(height: 5),
+                                      Container(
+                                        height: 12.0,
+                                        color: Colors.white,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
                     );
                   case Status.success:
                     return ListView.builder(
