dac31ab2049aea1d5de20cf2391a5d394e5a85a5
igorurbaniak <igorurbaniakq@wp.pl>
2024-08-20 22:00:32 +0200

    Improve CreateUserPage to control of sign up status and simultaneous change in UI

--- lib/presentation/features/auth/auth_pages/create_user_page/create_user_page.dart
+++ lib/presentation/features/auth/auth_pages/create_user_page/create_user_page.dart
@@ -1,3 +1,4 @@
+import 'package:cafe_of_happiness_app/app/core/enums/enums.dart';
 import 'package:cafe_of_happiness_app/app/custom_widgets/back_appbar.dart';
 import 'package:cafe_of_happiness_app/domain/repositories/auth_repository/auth_repository.dart';
 import 'package:cafe_of_happiness_app/presentation/features/auth/auth_pages/create_user_page/widgets/have_account_button.dart';
@@ -27,22 +28,24 @@ class _CreateUserPageState extends State<CreateUserPage> {
       create: (context) => AuthCubit(AuthRepository()),
       child: BlocConsumer<AuthCubit, AuthState>(
         listener: (context, state) {
-          if (state.isAuthenticated) {
+          if (state.status == Status.success) {
             Navigator.of(context).pushReplacement(
               MaterialPageRoute(
                 builder: (context) => HomePage(user: state.user),
               ),
             );
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(
                 content: Text(
-                  'Account successfully created!',
+                  'Account successfully created! Welcome!👋',
                   style: Theme.of(context).textTheme.titleSmall,
                 ),
                 backgroundColor: Theme.of(context).colorScheme.outline,
               ),
             );
-          } else if (state.errorMessage.isNotEmpty) {
+          } else if (state.status == Status.error) {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
             ScaffoldMessenger.of(context).showSnackBar(
               SnackBar(
                 content: Text(
@@ -59,45 +62,59 @@ class _CreateUserPageState extends State<CreateUserPage> {
             resizeToAvoidBottomInset: false,
             backgroundColor: Theme.of(context).colorScheme.surface,
             appBar: const BackAppBar(title: 'Café of Happiness'),
-            body: Container(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-              child: Column(
-                children: [
-                  const AuthLogoWithText(
-                    authLogoText: 'Sign up',
-                    assetWidth: 150,
-                    assetHeight: 150,
+            body: Stack(
+              children: [
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                  child: Column(
+                    children: [
+                      const AuthLogoWithText(
+                        authLogoText: 'Sign up',
+                        assetWidth: 150,
+                        assetHeight: 150,
+                      ),
+                      const SizedBox(height: 20),
+                      AuthEmailInput(
+                        onEmailChanged: (newEmail) {
+                          setState(() {
+                            email = newEmail;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 10),
+                      CreatePasswordInput(
+                        onPasswordChanged: (newPassword) {
+                          setState(() {
+                            password = newPassword;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 50),
+                      AuthButton(
+                        buttonText: 'Sign up',
+                        onPressed: () {
+                          if (email != null && password != null) {
+                            context.read<AuthCubit>().signUp(
+                                  email: email!,
+                                  password: password!,
+                                );
+                          }
+                        },
+                      ),
+                      const SizedBox(height: 70),
+                      const HaveAccountButton(),
+                    ],
                   ),
-                  const SizedBox(height: 20),
-                  AuthEmailInput(
-                    onEmailChanged: (newEmail) {
-                      setState(() {
-                        email = newEmail;
-                      });
-                    },
+                ),
+                if (state.status == Status.loading)
+                  Container(
+                    color: Colors.black.withOpacity(0.5),
+                    child: const Center(
+                      child: CircularProgressIndicator(),
+                    ),
                   ),
-                  const SizedBox(height: 10),
-                  CreatePasswordInput(
-                    onPasswordChanged: (newPassword) {
-                      setState(() {
-                        password = newPassword;
-                      });
-                    },
-                  ),
-                  const SizedBox(height: 50),
-                  AuthButton(
-                    buttonText: 'Sign up',
-                    onPressed: () {
-                      context.read<AuthCubit>().signUp(
-                            email: email!,
-                            password: password!,
-                          );
-                    },
-                  ),
-                  const SizedBox(height: 70),
-                  const HaveAccountButton(),
-                ],
-              ),
+              ],
             ),
           );
         },
